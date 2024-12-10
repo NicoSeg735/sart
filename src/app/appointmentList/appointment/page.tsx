827f70bb3cb1111b8b114affd8ffc7bba5f82a18
Agustin Irala <agustinirala240@gmail.com>
2024-12-10 20:49:51 -0300

    Adding State in appointment

--- src/app/appointmentList/appointment/page.tsx
+++ src/app/appointmentList/appointment/page.tsx
@@ -1,16 +1,31 @@
 const appointment = {
   date: '2024-11-28',
   clientName: 'John Doe',
-  vehicle: 'Toyota Corolla'
+  vehicle: 'Toyota Corolla',
+  state: 'Pendiente'
 }
 
 export default function Appointment() {
   return (
     <>
-      <h2 className="text-xl font-semibold">Turno: {appointment.date}</h2>
+      <h2 className="mb-4 text-2xl font-bold text-gray-800">
+        Turno: {appointment.date}
+      </h2>
+      <h1 className="mb-6 text-lg font-medium text-gray-600">
+        Estado del turno: {appointment.state}
+      </h1>
+
       <div className="flex w-full items-center justify-center space-x-8">
-        <div className="w-1/3 rounded-lg border">{appointment.clientName}</div>
-        <div className="w-1/3 rounded-lg border">{appointment.vehicle}</div>
+        <div className="w-1/3 rounded-lg border border-gray-300 bg-white p-4 text-center shadow-md">
+          <p className="text-lg font-semibold text-gray-700">
+            {appointment.clientName}
+          </p>
+        </div>
+        <div className="w-1/3 rounded-lg border border-gray-300 bg-white p-4 text-center shadow-md">
+          <p className="text-lg font-semibold text-gray-700">
+            {appointment.vehicle}
+          </p>
+        </div>
       </div>
     </>
   )
